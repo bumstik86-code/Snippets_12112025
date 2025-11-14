adb9fb9b0f86c35f923e44b7896283cd75bd0a37
Ivan Sivolobov <bumstik86@gmail.com>
2025-11-14 16:29:01 +0300

    Done: M9: admin resource

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from MainApp.models import Comment, Snippet
 
 # Register your models here.
+# admin.site.register([Snippet, Comment])
+
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ["name", "lang", "creation_date", "public", "code"]
+    list_filter = ["name", "creation_date"]
+    ordering = ["creation_date"]
+    search_fields = ["name", "code"]
+
+@admin.register(Comment)
+class SCommentAdmin(admin.ModelAdmin):
+    list_display = ["text"]
+    search_fields = ["text"]
